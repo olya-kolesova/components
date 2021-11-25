ae20fcdaa127ac4cbc0fdb0a89c5685d3a435371
Olga Kolesova <olka.kolesova@gmail.com>
2021-11-25 21:31:48 +0000

    little blog

--- src/index.js
+++ src/index.js
@@ -7,25 +7,36 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard/>
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 2:00 PM"
-        comment="Hey, guys!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 10:00 AM"
-        comment="Nice blog post!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 3:45 AM"
-        comment="What are you up to?"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          There is a blog below
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 2:00 PM"
+          comment="Hey, guys!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 10:00 AM"
+          comment="Nice blog post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 3:45 AM"
+          comment="What are you up to?"
+          avatar={faker.image.avatar()}
+        />
+        </ApprovalCard>
     </div>
   );
 };
